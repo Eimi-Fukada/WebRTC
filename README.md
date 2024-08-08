4c6c5395d5a3a7974a4d364baa70af585237d3a5
grace <1745282879@qq.com>
2024-08-08 13:55:06 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# WebRTC
-webRTC demo by vue
+# WebRTC 
+
+## express-webrtc
+这个是express的服务端应用，用来进行webrtc信令过程
+
+## vuee-webrtc
+这个是实现webrtc的客户端应用
---
+++ vue-webrtc/src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <div>22</div>
+</template>
